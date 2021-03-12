9289e9fc1ef4bc0cba447429b7d4d36087614ba0
george.thomas <georgethomas2897@yahoo.com>
2021-03-12 15:28:51 -0600

    Added humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,9 +8,8 @@ const forecast = (latitude,longitude, callback)=>{
         }else if(body.error){
             callback('Unable to find location. Try another search',undefined)
         }else{
-            callback(undefined,body.current.weather_descriptions[0]+". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out." )
+            callback(undefined,body.current.weather_descriptions[0]+". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out. The current humidity is currently " +body.current.humidity + "%" )
         }
     })
 }
-
 module.exports = forecast
